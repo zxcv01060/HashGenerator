325dd07c64dd96188a6bdc90a3ed99a1ff96cfb7
EnWei <10646007@ntub.edu.tw>
2021-09-22 21:13:43 +0800

    調整 - 新增演算法下拉選單項目

--- app/src/main/java/tw/idv/louisli/hashgenerator/view/fragment/HashGeneratorFragment.kt
+++ app/src/main/java/tw/idv/louisli/hashgenerator/view/fragment/HashGeneratorFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
 import tw.idv.louisli.hashgenerator.R
 
@@ -16,4 +18,16 @@ class HashGeneratorFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_hash_generator, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val textAlgorithm =
+            view.findViewById<AutoCompleteTextView>(R.id.text_hash_generator_algorithm_content)
+        textAlgorithm.setAdapter(
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_dropdown_item_1line,
+                resources.getStringArray(R.array.support_hash_algorithm)
+            )
+        )
+    }
 }
