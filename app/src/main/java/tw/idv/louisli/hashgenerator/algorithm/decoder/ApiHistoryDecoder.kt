aa6678ff5600944d900198a442c2972fc4168a66
EnWei <10646007@ntub.edu.tw>
2021-10-02 21:31:07 +0800

    修正 - 修正ApiHistoryDecoder執行錯誤的問題

--- app/src/main/java/tw/idv/louisli/hashgenerator/algorithm/decoder/ApiHistoryDecoder.kt
+++ app/src/main/java/tw/idv/louisli/hashgenerator/algorithm/decoder/ApiHistoryDecoder.kt
@@ -4,6 +4,8 @@ import android.util.Log
 import kotlinx.coroutines.suspendCancellableCoroutine
 import tw.idv.louisli.hashgenerator.BuildConfig
 import java.net.URL
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 object ApiHistoryDecoder : HashDecoder {
     private const val url = "https://md5decrypt.net/en/Api/api.php"
@@ -11,16 +13,24 @@ object ApiHistoryDecoder : HashDecoder {
     private const val code = BuildConfig.API_CODE
 
     override suspend fun decode(algorithm: String?, ciphertext: String): String? {
-        val response = suspendCancellableCoroutine<String> {
-            URL(
-                "$url?email=$email&code=$code&hash=$ciphertext&hash_type=${
-                    algorithm?.lowercase()?.replaceFirst("-", "") ?: ""
-                }"
-            ).readText()
+        val result = suspendCancellableCoroutine<String> {
+            try {
+                val url = URL(
+                    "$url?email=$email&code=$code&hash=$ciphertext&hash_type=${
+                        algorithm?.lowercase()?.replaceFirst("-", "") ?: ""
+                    }"
+                )
+                it.resume(url.readText())
+            } catch (e: Exception) {
+                e.printStackTrace()
+                it.resumeWithException(e)
+            }
         }
-
-        Log.d("ApiHistoryDecoder", response)
-
-        return null
+        return if (result.startsWith("ERROR CODE : ")) {
+            Log.e("ApiHistoryDecoder", "找不到對應的明文($algorithm)：$ciphertext")
+            null
+        } else {
+            result
+        }
     }
 }
