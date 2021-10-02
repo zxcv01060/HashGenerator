ba206ccf0972d30af82fd715203cb18be121b857
EnWei <10646007@ntub.edu.tw>
2021-10-02 21:45:26 +0800

    調整 - 在歷史記錄的地方增加分隔線

--- app/src/main/java/tw/idv/louisli/hashgenerator/view/bindingadapter/RecyclerViewBindingAdapter.kt
+++ app/src/main/java/tw/idv/louisli/hashgenerator/view/bindingadapter/RecyclerViewBindingAdapter.kt
@@ -1,7 +1,13 @@
 package tw.idv.louisli.hashgenerator.view.bindingadapter
 
+import android.content.Context
+import android.graphics.Color
+import androidx.annotation.ColorInt
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.color.MaterialColors
+import tw.idv.louisli.hashgenerator.R
+import tw.idv.louisli.hashgenerator.view.DividerItemDecoration
 import tw.idv.louisli.hashgenerator.view.adapter.AdapterItem
 import tw.idv.louisli.hashgenerator.view.adapter.DataBindingRecyclerViewAdapter
 import tw.idv.louisli.hashgenerator.view.adapter.viewholder.ViewHolderFactory
@@ -24,4 +30,26 @@ fun setItem(
     itemViewLongClickable: Boolean
 ) {
     view.adapter = DataBindingRecyclerViewAdapter(items, viewHolderFactory, itemViewLongClickable)
+}
+
+@BindingAdapter("enableDivider")
+fun setDivider(view: RecyclerView, isEnableDivider: Boolean) {
+    if (isEnableDivider) {
+        setDivider(view, null, null)
+    }
+}
+
+@BindingAdapter(value = ["dividerColor", "dividerHeight"], requireAll = false)
+fun setDivider(view: RecyclerView, @ColorInt color: Int?, height: Int?) {
+    @ColorInt
+    fun getDefaultColor(context: Context): Int {
+        return MaterialColors.getColor(context, R.attr.colorOnBackground, Color.GRAY)
+    }
+
+    view.addItemDecoration(
+        DividerItemDecoration(
+            color ?: getDefaultColor(view.context),
+            height ?: 2
+        )
+    )
 }
